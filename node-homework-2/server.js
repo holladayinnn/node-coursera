539cbd42365c4a3c48c1e958890ec080e824ed30
Tommy Holladay <thomaswholladay@gmail.com>
2016-07-29 17:15:39 -0400

    Node Coursera Class

---
+++ node-homework-2/server.js
@@ -0,0 +1,73 @@
+var mongoose = require('mongoose'),
+    assert = require('assert');
+
+var Dishes = require('./models/dishes');
+var Promotions = require('./models/promotions');
+var Leaderships = require('./models/leadership');
+// Connection URL
+var url = 'mongodb://localhost:27017/conFusion';
+mongoose.connect(url);
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function () {
+    // we're connected!
+    console.log("Connected correctly to server");
+    // create a new dish
+    Dishes.create({
+        name: 'Uthapizza',
+        image: 'images/uthapizza.png',
+        category: 'mains',
+        label: 'Hot',
+        price: '4.99',
+        description: 'A unique...',
+        comments: [
+            {
+                rating: 5,
+                comment: 'Imagine all the eatables living in conFusion!',
+                author: 'John Lemon'
+            },
+            {
+                rating: 4,
+                comment: 'Sends anyone to heaven.  I wish I could get my mother in law to eat it.',
+                author: 'Paul McVites'
+            }
+        ]
+    }, function (err, dish) {
+        if (err) throw err;
+        console.log(dish);
+
+        db.collection('dishes').drop();
+    });
+
+    Promotions.create({
+        name: 'Weekend Grand Buffet',
+        image: 'images/buffet.png',
+        label: 'New',
+        price: '19.99',
+        description: 'Featuring...',
+    }, function (err, promotion) {
+        if (err) throw err;
+        console.log(promotion);
+
+        db.collection('promotions').drop();
+    });
+
+    Leaderships.create({
+        name: 'Peter Pan',
+        image: 'images/alberto.png',
+        designation: 'Chief Epicurious Officer',
+        abbr: 'CEO',
+        description: 'Our CEO, Peter, ...',
+    }, function (err, leadership) {
+        if (err) throw err;
+        console.log(leadership);
+
+        db.collection('leaderships').drop();
+    });
+
+    setTimeout(function () {
+        db.close();
+    }, 1000)
+});
